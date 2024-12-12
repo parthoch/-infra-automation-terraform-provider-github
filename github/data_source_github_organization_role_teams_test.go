integrations/terraform-provider-github

feat: Added organization role support
Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

github/data_source_github_organization_role_teams_test.go
@@ -1,0 +1,104 @@
+package github
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceGithubOrganizationRoleTeams(t *testing.T) {
+	t.Run("get the organization role teams without error", func(t *testing.T) {
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		teamName := fmt.Sprintf("tf-acc-team-%s", randomID)
+		roleId := 8134
+		config := fmt.Sprintf(`
+			resource "github_team" "test" {
+				name = "%s"
+			}
+
+			resource "github_organization_role_team" "test" {
+				role_id   = %d
+				team_slug = github_team.test.slug
+			}
+
+			data "github_organization_role_teams" "test" {
+				role_id = %[2]d
+
+				depends_on = [
+					github_organization_role_team.test
+				]
+			}
+		`, teamName, roleId)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { skipUnlessMode(t, organization) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttrSet("data.github_organization_role_teams.test", "teams.#"),
+						resource.TestCheckResourceAttr("data.github_organization_role_teams.test", "teams.#", "1"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.id", "github_team.test", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.slug", "github_team.test", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.name", "github_team.test", "name"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("get indirect organization role teams without error", func(t *testing.T) {
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		teamName1 := fmt.Sprintf("tf-acc-team-1-%s", randomID)
+		teamName2 := fmt.Sprintf("tf-acc-team-2-%s", randomID)
+		roleId := 8134
+		config := fmt.Sprintf(`
+			resource "github_team" "test_1" {
+				name    = "%s"
+				privacy = "closed"
+			}
+
+			resource "github_team" "test_2" {
+				name           = "%s"
+				privacy        = "closed"
+				parent_team_id = github_team.test_1.id
+			}
+
+			resource "github_organization_role_team" "test" {
+				role_id   = %d
+				team_slug = github_team.test_1.slug
+			}
+
+			data "github_organization_role_teams" "test" {
+				role_id = %[3]d
+
+				depends_on = [
+					github_organization_role_team.test
+				]
+			}
+		`, teamName1, teamName2, roleId)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { skipUnlessMode(t, organization) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttrSet("data.github_organization_role_teams.test", "teams.#"),
+						resource.TestCheckResourceAttr("data.github_organization_role_teams.test", "teams.#", "2"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.id", "github_team.test_1", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.slug", "github_team.test_1", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.name", "github_team.test_1", "name"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.id", "github_team.test_2", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.slug", "github_team.test_2", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.name", "github_team.test_2", "name"),
+					),
+				},
+			},
+		})
+	})
+}

github/resource_github_organization_role_user.go
@@ -1,0 +1,136 @@
+package github
+
+import (
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/google/go-github/v66/github"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceGithubOrganizationRoleUser() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGithubOrganizationRoleUserCreate,
+		Read:   resourceGithubOrganizationRoleUserRead,
+		Delete: resourceGithubOrganizationRoleUserDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"role_id": {
+				Description: "The unique identifier of the organization role.",
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"login": {
+				Description: "The handle for the GitHub user account.",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func resourceGithubOrganizationRoleUserCreate(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleId := int64(d.Get("role_id").(int))
+	login := d.Get("login").(string)
+
+	_, err = client.Organizations.AssignOrgRoleToUser(ctx, orgName, login, roleId)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(buildTwoPartID(strconv.FormatInt(roleId, 10), login))
+
+	return resourceGithubOrganizationRoleUserRead(d, meta)
+}
+
+func resourceGithubOrganizationRoleUserRead(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleIdString, login, err := parseTwoPartID(d.Id(), "role_id", "login")
+	if err != nil {
+		return err
+	}
+	roleId, err := strconv.ParseInt(roleIdString, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	opts := &github.ListOptions{
+		PerPage: maxPerPage,
+	}
+
+	var user *github.User
+	for {
+		users, resp, err := client.Organizations.ListUsersAssignedToOrgRole(ctx, orgName, roleId, opts)
+		if err != nil {
+			return err
+		}
+
+		for _, u := range users {
+			if u.GetLogin() == login {
+				user = u
+				break
+			}
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	if user == nil {
+		log.Printf("[INFO] Removing organization role User (%d:%s) from state because it no longer exists in GitHub", roleId, login)
+		d.SetId("")
+		return nil
+	}
+
+	if err = d.Set("role_id", roleId); err != nil {
+		return err
+	}
+
+	if err = d.Set("login", login); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceGithubOrganizationRoleUserDelete(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleId := int64(d.Get("role_id").(int))
+	login := d.Get("login").(string)
+
+	_, err = client.Organizations.RemoveOrgRoleFromUser(ctx, orgName, login, roleId)
+	return err
+}

parthoch/-infra-automation-terraform-provider-github

github/data_source_github_organization_role_teams_test.go
@@ -1,0 +1,104 @@
+package github
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceGithubOrganizationRoleTeams(t *testing.T) {
+	t.Run("get the organization role teams without error", func(t *testing.T) {
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		teamName := fmt.Sprintf("tf-acc-team-%s", randomID)
+		roleId := 8134
+		config := fmt.Sprintf(`
+			resource "github_team" "test" {
+				name = "%s"
+			}
+
+			resource "github_organization_role_team" "test" {
+				role_id   = %d
+				team_slug = github_team.test.slug
+			}
+
+			data "github_organization_role_teams" "test" {
+				role_id = %[2]d
+
+				depends_on = [
+					github_organization_role_team.test
+				]
+			}
+		`, teamName, roleId)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { skipUnlessMode(t, organization) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttrSet("data.github_organization_role_teams.test", "teams.#"),
+						resource.TestCheckResourceAttr("data.github_organization_role_teams.test", "teams.#", "1"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.team_id", "github_team.test", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.slug", "github_team.test", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.name", "github_team.test", "name"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("get indirect organization role teams without error", func(t *testing.T) {
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		teamName1 := fmt.Sprintf("tf-acc-team-1-%s", randomID)
+		teamName2 := fmt.Sprintf("tf-acc-team-2-%s", randomID)
+		roleId := 8134
+		config := fmt.Sprintf(`
+			resource "github_team" "test_1" {
+				name    = "%s"
+				privacy = "closed"
+			}
+
+			resource "github_team" "test_2" {
+				name           = "%s"
+				privacy        = "closed"
+				parent_team_id = github_team.test_1.id
+			}
+
+			resource "github_organization_role_team" "test" {
+				role_id   = %d
+				team_slug = github_team.test_1.slug
+			}
+
+			data "github_organization_role_teams" "test" {
+				role_id = %[3]d
+
+				depends_on = [
+					github_organization_role_team.test
+				]
+			}
+		`, teamName1, teamName2, roleId)
+
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { skipUnlessMode(t, organization) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttrSet("data.github_organization_role_teams.test", "teams.#"),
+						resource.TestCheckResourceAttr("data.github_organization_role_teams.test", "teams.#", "2"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.team_id", "github_team.test_1", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.slug", "github_team.test_1", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.0.name", "github_team.test_1", "name"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.team_id", "github_team.test_2", "id"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.slug", "github_team.test_2", "slug"),
+						resource.TestCheckResourceAttrPair("data.github_organization_role_teams.test", "teams.1.name", "github_team.test_2", "name"),
+					),
+				},
+			},
+		})
+	})
+}

github/resource_github_organization_role_user.go
@@ -1,0 +1,138 @@
+package github
+
+import (
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/google/go-github/v66/github"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceGithubOrganizationRoleUser() *schema.Resource {
+	return &schema.Resource{
+		Description: "Manage an association between an organization role and a user.",
+
+		Create: resourceGithubOrganizationRoleUserCreate,
+		Read:   resourceGithubOrganizationRoleUserRead,
+		Delete: resourceGithubOrganizationRoleUserDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"role_id": {
+				Description: "The unique identifier of the organization role.",
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"login": {
+				Description: "The login for the GitHub user account.",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func resourceGithubOrganizationRoleUserCreate(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleId := int64(d.Get("role_id").(int))
+	login := d.Get("login").(string)
+
+	_, err = client.Organizations.AssignOrgRoleToUser(ctx, orgName, login, roleId)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(buildTwoPartID(strconv.FormatInt(roleId, 10), login))
+
+	return resourceGithubOrganizationRoleUserRead(d, meta)
+}
+
+func resourceGithubOrganizationRoleUserRead(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleIdString, login, err := parseTwoPartID(d.Id(), "role_id", "login")
+	if err != nil {
+		return err
+	}
+	roleId, err := strconv.ParseInt(roleIdString, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	opts := &github.ListOptions{
+		PerPage: maxPerPage,
+	}
+
+	var user *github.User
+	for {
+		users, resp, err := client.Organizations.ListUsersAssignedToOrgRole(ctx, orgName, roleId, opts)
+		if err != nil {
+			return err
+		}
+
+		for _, u := range users {
+			if u.GetLogin() == login {
+				user = u
+				break
+			}
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	if user == nil {
+		log.Printf("[INFO] Removing organization role User (%d:%s) from state because it no longer exists in GitHub", roleId, login)
+		d.SetId("")
+		return nil
+	}
+
+	if err = d.Set("role_id", roleId); err != nil {
+		return err
+	}
+
+	if err = d.Set("login", login); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceGithubOrganizationRoleUserDelete(d *schema.ResourceData, meta interface{}) error {
+	err := checkOrganization(meta)
+	if err != nil {
+		return err
+	}
+
+	client := meta.(*Owner).v3client
+	ctx := context.Background()
+	orgName := meta.(*Owner).name
+
+	roleId := int64(d.Get("role_id").(int))
+	login := d.Get("login").(string)
+
+	_, err = client.Organizations.RemoveOrgRoleFromUser(ctx, orgName, login, roleId)
+	return err
+}